ef3572cff98e1cb536fa1d85d7d64c01df15b3bc
David Arcís Moreno <szikra.dev@gmail.com>
2024-04-06 00:06:24 +0200

    Adds Pipes and random spawn first approach

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ push = require 'push'
 Class = require 'class'
 
 require 'Bird'
+require 'Pipe'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -22,6 +23,10 @@ local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
 
+local pipes = {}
+
+local spawnTimer = 0
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -32,6 +37,8 @@ function love.load()
         resizable = true,
         vsync = true
     })
+
+    math.randomseed(os.time())
     -- we init a custom table in the love.keyboard
     love.keyboard.keysPressed = {}
 end
@@ -57,7 +64,22 @@ function love.update(dt)
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
+    spawnTimer = spawnTimer + dt
+
+    if spawnTimer > 2 then
+        table.insert(pipes, Pipe())
+        spawnTimer = 0
+    end
+
     bird:update(dt)
+    -- for each key/value pair of pipes
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+
+        if (pipe.x < -pipe.width) then
+            table.remove(pipes, k)
+        end
+    end
 
     -- reset so we can listen for keys avery frame
     love.keyboard.keysPressed = {}
@@ -67,6 +89,10 @@ function love.draw()
     push:start()
 
     love.graphics.draw(background, -backgroundScroll, 0)
+
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
 
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
