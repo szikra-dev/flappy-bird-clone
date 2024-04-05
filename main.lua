40b323aaaccf6f21a00c2bcde73601f6591f379e
David Arcís Moreno <szikra.dev@gmail.com>
2024-04-05 22:50:13 +0200

    Adds parallax effect

--- main.lua
+++ main.lua
@@ -7,7 +7,15 @@ VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -25,6 +33,11 @@ function love.resize(w, h)
     push:resize(w, h)
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+end
+
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
@@ -34,9 +47,9 @@ end
 function love.draw()
     push:start()
 
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
 
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     push:finish()
 end
