f77740058b04461ce81777e29e5f9d757e731717
David Arcís Moreno <szikra.dev@gmail.com>
2024-04-06 02:03:15 +0200

    Adds game state to main

--- main.lua
+++ main.lua
@@ -5,6 +5,11 @@ require 'Bird'
 require 'Pipe'
 require 'PipePair'
 
+require 'StateMachine'
+require 'states/BaseState'
+require 'states/PlayState'
+require 'states/TitleScreenState'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -23,87 +28,52 @@ local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
 local GROUND_LOOPING_POINT = 514
 
-local bird = Bird()
-
-local pipePairs = {}
-
-local spawnTimer = 0
-local lastY = -PIPE_HEIGHT + math.random(80) + 20
-
-local scrolling = true
-
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     love.window.setTitle('Flappy Bird')
 
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    mediumFont = love.graphics.newFont('flappy.ttf', 14)
+    flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('flappy.ttf', 56)
+    love.graphics.setFont(flappyFont)
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = true,
         vsync = true
-    })
+     })
 
     math.randomseed(os.time())
+
+    -- initialize state machine with all state-returning functions
+    gStateMachine = StateMachine {
+        ['title'] = function() return TitleScreenState() end,
+        ['play'] = function() return PlayState() end
+     }
+    gStateMachine:change('title')
+
     -- we init a custom table in the love.keyboard
     love.keyboard.keysPressed = {}
 end
 
-function love.resize(w, h)
-    push:resize(w, h)
-end
+function love.resize(w, h) push:resize(w, h) end
 
 function love.keypressed(key)
     love.keyboard.keysPressed[key] = true
 
-    if key == 'escape' then
-        love.event.quit()
-    end
+    if key == 'escape' then love.event.quit() end
 end
 
 -- Adding a custom function to the love.keyboard
-function love.keyboard.wasPressed(key)
-    return love.keyboard.keysPressed[key]
-end
+function love.keyboard.wasPressed(key) return love.keyboard.keysPressed[key] end
 
 function love.update(dt)
-    if scrolling then
-        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
 
-        spawnTimer = spawnTimer + dt
-
-        if spawnTimer > 2 then
-            local y = math.max(-PIPE_HEIGHT + 10,
-                math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-
-            lastY = y
-
-            table.insert(pipePairs, PipePair(y))
-            spawnTimer = 0
-        end
-
-        bird:update(dt)
-        -- for each key/value pair of pipes
-        for k, pair in pairs(pipePairs) do
-            pair:update(dt)
-
-            for l, pipe in pairs(pair.pipes) do
-                if bird:collides(pipe) then
-                    scrolling = false
-                end
-            end
-
-            if pair.x < -PIPE_WIDTH then
-                pair.remove = true
-            end
-        end
-
-        for k, pair in pairs(pipePairs) do
-            if pair.remove then
-                table.remove(pipePairs, k)
-            end
-        end
-    end
+    gStateMachine:update(dt)
 
     -- reset so we can listen for keys avery frame
     love.keyboard.keysPressed = {}
@@ -113,14 +83,8 @@ function love.draw()
     push:start()
 
     love.graphics.draw(background, -backgroundScroll, 0)
-
-    for k, pair in pairs(pipePairs) do
-        pair:render()
-    end
-
+    gStateMachine:render()
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
-
-    bird:render()
 
     push:finish()
 end
